9c12a46572312052db26de07ba4cb37b99eb89e9
gumi <97876768+necoarcdotcom@users.noreply.github.com>
2023-08-30 10:10:12 -0500

    other branch exists

--- opencore.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-REM a new beginning
-REM colors here
-temp_dir=$(mktemp -d)
-title OpenCore EFI Maker Made with love by Neco Arc (gumi) and Mac <33
-echo Welcome to OpenCore EFI Maker! Now, we'll start by downloading some files.
-rem will continue later
