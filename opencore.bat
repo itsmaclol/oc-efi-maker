dde5b747baf7e1cd1fe619b93f9b208cee4770db
gumi <spenis7613@gmail.com>
2023-08-30 10:08:18 -0500

    idk how to commit to the other branch >.<

--- opencore.bat
+++ opencore.bat
@@ -2,6 +2,6 @@
 REM a new beginning
 REM colors here
 temp_dir=$(mktemp -d)
-title OpenCore EFI Maker Made with love by Neco Arc (gumi) and Mac <33
+title OpenCore EFI Maker Made with love by Neco Arc (gumi) and Mac <3
 echo Welcome to OpenCore EFI Maker! Now, we'll start by downloading some files.
 rem will continue later
