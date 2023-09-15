d0571c61ce69e6ddc6dba341997091764ef9e86c
gumi <spenis7613@gmail.com>
2023-09-15 13:28:45 -0500

    complete server plists :3

--- opencore.sh
+++ opencore.sh
@@ -5071,6 +5071,165 @@ amd1719_desktop_config_setup(){
     warning "Please enable the following options in the BIOS.\nAbove 4G Decoding\nEHCI/XHCI Hand-off\nOS type: Windows 8.1/10 UEFI Mode (might be Other OS)\nSATA Mode: AHCI"
 }
 
+haswell_broadwell_e_server_config_setup(){
+        vtd() {
+        echo "################################################################"
+        echo "Is VT-D enabled in BIOS?"
+        echo "################################################################"
+        read -r -p "y/n: " vtd_choice
+        case $vtd_choice in
+            y|Y|YES|yes|Yes )
+                set_plist Kernel.Quirks.DisableIoMapper bool True
+            ;;
+            N|n|NO|No|no )
+                echo "" > /dev/null
+            ;;
+            * )
+                error "Invalid Choice"
+                vtd
+        esac
+    }
+    hpmachine(){
+        echo "################################################################"
+        echo "Do you have a HP System?"
+        echo "################################################################"
+        read -r -p "y/n: " hp_choice
+        case $hp_choice in
+            y|Y|YES|yes|Yes )
+                set_plist Kernel.Quirks.LapicKernelPanic bool True
+            ;;
+            n|N|NO|no|No )
+                echo "" > /dev/null
+            ;;
+            * )
+                error "Invalid Choice"
+                hpmachine
+            esac
+    }
+    set_plist Kernel.Emulate.Cpuid1Data data "c3060300 00000000 00000000 00000000"
+    set_plist Kernel.Emulate.Cpuid1Mask data "ffffffff 00000000 00000000 00000000"
+    set_plist Kernel.Quirks.AppleXcpmCfgLock bool True
+    set_plist Kernel.Quirks.AppleXcpmExtraMsrs bool True
+    set_plist Kernel.Quirks.PanicNoKextDump bool True
+    set_plist Kernel.Quirks.PowerTimeoutKernelPanic bool True
+    case $os_choice in
+        4|5 ) 
+            set_plist Kernel.Quirks.XhciPortLimit bool False
+        ;;
+    esac
+    set_plist Misc.Debug.AppleDebug bool True
+    set_plist Misc.Debug.ApplePanic bool True
+    set_plist Misc.Debug.DisableWatchDog bool True
+    set_plist Misc.Debug.Target int 67
+    set_plist Misc.Security.AllowSetDefault bool True
+    set_plist Misc.Security.BlacklistAppleUpdate bool True
+    set_plist Misc.Security.ScanPolicy int 0
+    set_plist Misc.Security.SecureBootModel string Default
+    set_plist Misc.Security.Vault string Optional
+    set_plist NVRAM.Add.7C436110-AB2A-4BBB-A880-FE41995C9F82.boot-args string "-v debug=0x100 alcid=1 keepsyms=1"
+    # gpu args go here
+    set_plist NVRAM.Delete.LegacyOverwrite bool True
+    smbiosname=iMacPro1,1
+    macserial
+    case $os_choice in
+        4 )
+            set_plist UEFI.APFS.MinVersion int 1412101001000000
+            set_plist UEFI.APFS.MinDate int 20200306
+        ;;
+        5 )
+            set_plist UEFI.APFS.MinVersion int 945275007000000
+            set_plist UEFI.APFS.MinDate int 20190820
+        ;;
+    esac
+    set_plist UEFI.Quirks.IgnoreInvalidFlexRatio bool True
+    hpmachine
+    vtd
+    info "Done!"
+    info "Your EFI is located at $dir/EFI"
+    warning "Please disable the following options in the BIOS.\nFast Boot\nSecure Boot\nSerial/COM Port\nParallel Port\nVT-d\nCompatibility Support Module (CSM)\nThunderbolt (For intital install)\nIntel SGX\nIntel Platform Trust\nCFG Lock"
+    warning "Please enable the following options in the BIOS.\nVT-x\nAbove 4G Decoding/Hyper Threading (If experiencing issues, ensure "MMIOH Base" is set to 12 TB or lower)\nExecute Disable Bit\nEHCI/XHCI Hand-off\nOS type: Windows 8.1/10 UEFI Mode (might be Other OS)\nSATA Mode: AHCI"
+}
+
+skylake_cascade_server_config_setup(){
+    set_plist Booter.Quirks.DevirtualiseMmio bool True
+    set_plist Booter.Quirks.EnableWriteUnprotector bool False
+    set_plist Booter.Quirks.RebuildAppleMemoryMap bool True
+    virtuamap(){
+        echo "################################################################"
+        echo "Do you have an ASUS system and is your BIOS version v3006 or higher?"
+        echo "################################################################"
+        read -r -p "y/n: " virtuamap_choice
+        case $virtuamap_choice in
+            y|Y|YES|Yes|yes )
+                set_plist Booter.Quirks.SetupVirtualMap bool True
+            ;;
+            n|N|NO|No|no )
+                echo "" > /dev/null
+            ;;
+            * )
+                error "Invalid Choice"
+                virtuamap
+            esac
+    }
+    virtuamap
+    #gumi note: me when virtua fighter reference
+    set_plist Booter.Quirks.SyncRuntimePermissions bool True
+    set_plist Kernel.Quirks.AppleXcpmCfgLock bool True
+    set_plist Kernel.Quirks.DisableIoMapper bool True
+    hpserver(){
+        echo "################################################################"
+        echo "Do you have a HP System?"
+        echo "################################################################"
+        read -r -p "y/n: " hpserver_choice
+        case $hpserver_choice in
+            y|Y|YES|Yes|yes )
+                set_plist Kernel.Quirks.LapicKernelPanic bool True
+            ;;
+            n|N|NO|No|no )
+                echo "" > /dev/null
+            ;;
+            * )
+                error "Invalid Choice"
+                hpserver
+            esac
+    }
+    set_plist Kernel.Quirks.PanicNoKextDump bool True
+    set_plist Kernel.Quirks.PowerTimeoutKernelPanic bool True
+    case $os_choice in
+        4|5 ) 
+            set_plist Kernel.Quirks.XhciPortLimit bool False
+        ;;
+    esac
+    set_plist Misc.Debug.AppleDebug bool True
+    set_plist Misc.Debug.ApplePanic bool True
+    set_plist Misc.Debug.DisableWatchDog bool True
+    set_plist Misc.Debug.Target int 67
+    set_plist Misc.Security.AllowSetDefault bool True
+    set_plist Misc.Security.BlacklistAppleUpdate bool True
+    set_plist Misc.Security.ScanPolicy int 0
+    set_plist Misc.Security.SecureBootModel string Default
+    set_plist Misc.Security.Vault string Optional
+    set_plist NVRAM.Add.7C436110-AB2A-4BBB-A880-FE41995C9F82.boot-args string "-v debug=0x100 alcid=1 keepsyms=1"
+    # gpu args go here
+    smbiosname=iMacPro1,1
+    macserial
+    case $os_choice in
+        4 )
+            set_plist UEFI.APFS.MinVersion int 1412101001000000
+            set_plist UEFI.APFS.MinDate int 20200306
+        ;;
+        5 )
+            set_plist UEFI.APFS.MinVersion int 945275007000000
+            set_plist UEFI.APFS.MinDate int 20190820
+        ;;
+    esac
+    hpserver
+    info "Done!"
+    info "Your EFI is located at $dir/EFI"
+    warning "Please disable the following options in the BIOS.\nFast Boot\nSecure Boot\nSerial/COM Port\nParallel Port\nVT-d\nCompatibility Support Module (CSM)\nThunderbolt (For intital install)\nIntel SGX\nIntel Platform Trust\nCFG Lock"
+    warning "Please enable the following options in the BIOS.\nVT-x\nAbove 4G Decoding/Hyper Threading (If experiencing issues, ensure "MMIOH Base" is set to 12 TB or lower)\nExecute Disable Bit\nEHCI/XHCI Hand-off\nOS type: Windows 8.1/10 UEFI Mode (might be Other OS)\nSATA Mode: AHCI"
+}
+
 cpu_rev_laptop() {
     echo "################################################################"
     echo "Now, we need to ask you what generation your processor is."
@@ -5162,11 +5321,8 @@ cpu_rev_server(){
     echo "################################################################"
     read -r -p "Pick a number 1-3: " server_cpu_gen_choice
     case $server_cpu_gen_choice in
-        1 )
-            haswell_e_server_config_setup
-        ;;
-        2 )
-            broadwell_e_server_config_setup
+        1|2 )
+            haswell_broadwell_e_server_config_setup
         ;;
         3 )
             skylake_cascade_server_config_setup
